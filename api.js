6a1418e59ea91fee2f72c37c53dfeffc37dc6f32
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-07 10:56:52 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,9 @@
+async function api(action, payload = {}) {
+  const res = await fetch(GAS_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ action, ...payload })
+  });
+  return res.json();
+}
 
