3a273b936d70ce076770a6a9666069d81f79f36e
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-10 13:26:21 +0900

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-async function api(action, payload = {}) {
-  const res = await fetch(GAS_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ action, ...payload })
-  });
-  return res.json();
-}
-
