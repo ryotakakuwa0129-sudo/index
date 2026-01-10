76a6ed0a7769e2bf30baa91ea7faa18e886d8c22
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-10 14:57:37 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,10 +1,8 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
-
-async function api(action, data = {}) {
-  const res = await fetch(GAS_URL, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ action, ...data })
+async function api(action,data){
+  const res = await fetch(GAS_URL,{
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
+    body:JSON.stringify({ action, ...data })
   });
-  return res.json();
+  return await res.json();
 }
