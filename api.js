9e6c2aa1d30c7c26bfc37a989bc51486c61b1aad
ryotakakuwa0129-sudo <ryotakakuwa0129@gmail.com>
2026-01-12 11:46:21 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,14 +1,23 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycby0tjXYVUWyPRwqs7r7PwJrrslfTCdZIeQmFwwT1JUfMF9N4a6XwXtgvMz-JDIzIt_mxQ/exec";
+const GAS_URL = "GAS_URL";
 
 async function post(data) {
   const res = await fetch(GAS_URL, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(data)
   });
+
+  if (!res.ok) {
+    throw new Error("HTTP error " + res.status);
+  }
+
   return res.json();
 }
 
 async function getUserId() {
-  const profile = await liff.getProfile();
-  return profile.userId;
+  // LINEログインチャンネル対応
+  const idToken = liff.getDecodedIDToken();
+  return idToken.sub; // ← これが userId
 }
